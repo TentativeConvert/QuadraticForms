3995bc7a518210074d57093ad084681e6f902f76
Marc Masdeu <marc.masdeu@gmail.com>
2023-09-05 17:15:04 +0200

    More test code

--- QuadraticForms/Test.lean
+++ QuadraticForms/Test.lean
@@ -1,14 +1,45 @@
 import Mathlib.LinearAlgebra.QuadraticForm.Basic
 
+open Real
+
 variable (K : Type) [Field K]
 variable (m : ℕ)
+
 example : Module K ((Fin m) → K) := by
   exact Pi.module (Fin m) (fun _ => K) K
 
-def q : QuadraticForm K (Fin m → K) where
-  toFun := _
-  toFun_smul := _
-  exists_companion' := _
+def example_qf : QuadraticForm K (Fin m → K) where
+  toFun := sorry
+  toFun_smul := sorry
+  exists_companion' := sorry
 
--- def WittMonoid := { nq : ∀ (n : ℕ), (QuadraticForm K (Fin n → K)) }
+structure QuadForms' where
+  M : Type
+  is_add_comm_monoid : AddCommMonoid M
+  is_module : Module K M
+  q : QuadraticForm K M
+
+
+example (T : Type) [AddCommMonoid T] (x y : T) : x + y = 0 := by
+  sorry
+done
+
+structure QuadForms where
+  n : ℕ
+  q : QuadraticForm K (Fin n → K)
+
+instance : Setoid (QuadForms K) where
+  r := 
+  iseqv := sorry
+
+#check QuadForms
+
+
+instance QuadFormsMonoid : AddCommMonoid (QuadForms K) where
+  add := _
+  add_assoc := _
+  zero := _
+  zero_add := _
+  add_zero := _
+  add_comm := _
 
