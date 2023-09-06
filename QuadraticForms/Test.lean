564d5c57632f45e8ece54e6e17f072b0f96792f3
Marc Masdeu <marc.masdeu@gmail.com>
2023-09-06 10:28:53 +0200

    Finished definition of MyNat and of bijection

--- QuadraticForms/Test.lean
+++ QuadraticForms/Test.lean
@@ -1,17 +1,65 @@
 import Mathlib.LinearAlgebra.QuadraticForm.Basic
+import Mathlib.LinearAlgebra.FiniteDimensional
 
-open Real
-
-variable (K : Type) [Field K]
+variable (K L : Type) [Field K] [Field L]
 variable (m : ℕ)
 
 example : Module K ((Fin m) → K) := by
   exact Pi.module (Fin m) (fun _ => K) K
 
+open scoped LinearMap
+open FiniteDimensional
+open Set
+
+
+structure FinDimVecSpaces (K : Type) [Field K] where
+  carrier : Type
+  isAddCommGroup : AddCommGroup carrier
+  isModule : Module K carrier
+  isFinDim : FiniteDimensional K carrier
+
+attribute [instance] FinDimVecSpaces.isAddCommGroup FinDimVecSpaces.isModule
+--  FinDimVecSpaces.isFinDim
+
+
+def VecSpacesUptoEquiv : Setoid ( FinDimVecSpaces K) where
+  r := λ V W ↦ Nonempty (V.carrier ≃ₗ[K] W.carrier)
+  --(finrank K V.carrier = finrank K W.carrier)
+  iseqv := {
+    refl := sorry
+    symm := sorry
+    trans := sorry --fun hx hy => hx.trans hy
+  }
+
+def MyNat := Quotient (VecSpacesUptoEquiv K)
+
+def MyKn (n : ℕ) : FinDimVecSpaces K := ⟨(Fin n → K), by exact Pi.addCommGroup, by exact Pi.module (Fin n) (fun _ => K) K, by exact
+  Module.Finite.pi ⟩
+
+#check MyKn
+
+example : FinDimVecSpaces K := MyKn K 3
+
+#check VecSpacesUptoEquiv K
+
+example (n : ℕ) : MyNat K := Quotient.mk (VecSpacesUptoEquiv K) (MyKn K n)
+
+
+def F : ℕ → MyNat K := λ n ↦ Quotient.mk (VecSpacesUptoEquiv K) (MyKn K n)
+
+theorem F_bijection : Function.Bijective (F K):= by
+  constructor
+  · sorry
+  · sorry
+done
+
+
+
 def example_qf : QuadraticForm K (Fin m → K) where
   toFun := sorry
   toFun_smul := sorry
   exists_companion' := sorry
+
 
 structure QuadForms' where
   M : Type
@@ -33,13 +81,4 @@ instance : Setoid (QuadForms K) where
   iseqv := sorry
 
 #check QuadForms
-
-
-instance QuadFormsMonoid : AddCommMonoid (QuadForms K) where
-  add := _
-  add_assoc := _
-  zero := _
-  zero_add := _
-  add_zero := _
-  add_comm := _
 
